be1349f8829240b28769d5434c1e3be53b36cd78
Alex Fornuto <alex@selfid.com>
2025-01-22 13:27:52 -0600

    add copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -160,6 +160,7 @@ const config = {
           },
         ],*/
         //copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `© 2024 SELF ID, Inc.`,
       },
       prism: {
         theme: prismThemes.github,
